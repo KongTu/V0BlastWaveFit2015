1c8cd1d24f70eeb6cb36a270667d89a3b5fa5915
KongTu <kongkong@Kongs-Mac-mini.local>
2015-02-06 10:02:59 -0600

    draw the fit to data ratio

--- MinuitBlastWaveCMSdataFit.C
+++ MinuitBlastWaveCMSdataFit.C
@@ -9,6 +9,7 @@
 #include "TF1.h"
 #include "TH1D.h"
 #include "TFile.h"
+#include "makeMultiPanelCanvas.C"
 
 #include "fitting.h"
 
@@ -124,8 +125,7 @@ void MinuitBlastWaveCMSdataFit(){
 
   gStyle->SetErrorX(0);
 
-
-  TFile* file = new TFile("/Users/kongkong/2015Research/Spectra/pPb2013/integratedEta/files/8MultSpectra_EPOS_5M_midRpy_v3_26ksbins_pt.root");
+  TFile* file = new TFile("/Users/kongkong/2015Research/Spectra/pPb2013/rpyDependence/files/MidRpy0_0.8_v1.root");
   
   TH1D* ksSpectra[8];
   TH1D* laSpectra[8];
@@ -150,13 +150,13 @@ void MinuitBlastWaveCMSdataFit(){
 
   }
 
-  double ks_ptbins[29] = {0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.4,3.8,4.2,4.6,5.0,5.6,6.6,9.0};
-    double ks_ptbincenter[26] = {0.2,0.35,0.45,0.55,0.65,0.75,0.85,0.95,1.1,1.3,1.5,1.7,1.9,2.1,2.3,2.5,2.7,2.9,3.2,3.6,4.0,4.4,4.8,5.3,6.1,7.8};
+  double ks_ptbins[26] = {0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.4,3.8,4.2,4.6,5.0,5.6,6.6,9.0};
+    double ks_ptbincenter[25] = {0.35,0.45,0.55,0.65,0.75,0.85,0.95,1.1,1.3,1.5,1.7,1.9,2.1,2.3,2.5,2.7,2.9,3.2,3.6,4.0,4.4,4.8,5.3,6.1,7.8};
   double la_ptbins[21] = {0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.4,3.8,4.2,4.6,5.0,5.6,6.6,9.0};
     double la_ptbincenter[20] = {0.7,0.9,1.1,1.3,1.5,1.7,1.9,2.1,2.3,2.5,2.7,2.9,3.2,3.6,4.0,4.4,4.8,5.3,6.1,7.8};
 
   TCanvas* c1 = new TCanvas();
-    c1->Divide(3,3,0,0);
+    c1->Divide(4,2,0,0);
 
   double beta_s[8],Tkin[8],aka[8],aka1[8],n[8];
   double ebeta_s[8],eTkin[8],eaka[8],eaka1[8],en[8];
@@ -167,6 +167,32 @@ void MinuitBlastWaveCMSdataFit(){
   TF1* f1[8];
   TF1* f2[8];
 
+  TH1D* f1_hist[8];
+  TH1D* f2_hist[8];
+
+  stringstream f1HistName;
+  stringstream f2HistName;
+
+  double ks_ptbins_histo[28] = {0.0,0.1,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.4,3.8,4.2,4.6,5.0,5.6,6.6,9.0};
+  double la_ptbins_histo[22] = {0.0,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.4,3.8,4.2,4.6,5.0,5.6,6.6,9.0};
+
+
+  for(int mult = 0; mult < 8; mult++){
+
+    f1HistName.str("");
+    f2HistName.str("");
+
+    f1HistName << "ks_fitFuncHist_";
+    f1HistName << mult+1;
+
+    f2HistName << "la_fitFuncHist_";
+    f2HistName << mult+1;
+
+    f1_hist[mult] = new TH1D(f1HistName.str().c_str(),f1HistName.str().c_str(),27,ks_ptbins_histo);
+    f2_hist[mult] = new TH1D(f2HistName.str().c_str(),f2HistName.str().c_str(),21,la_ptbins_histo);
+
+  }
+
   TLatex* ratio[8];
   ratio[0] = new TLatex(2.5,45,"0 < N^{offline}_{trk} < 35");
   ratio[1] = new TLatex(2.5,45,"35 < N^{offline}_{trk} < 60");
@@ -175,8 +201,13 @@ void MinuitBlastWaveCMSdataFit(){
   ratio[4] = new TLatex(2.5,45,"120 < N^{offline}_{trk} < 150");
   ratio[5] = new TLatex(2.5,45,"150 < N^{offline}_{trk} < 185");
   ratio[6] = new TLatex(2.5,45,"185 < N^{offline}_{trk} < 220");
-  ratio[7] = new TLatex(2.5,45,"220 < N^{offline}_{trk}");
+  ratio[7] = new TLatex(2.5,45,"220 < N^{offline}_{trk} < #infty");
   //ratio[8] = new TLatex(2.5,45,"N^{offline}_{trk} > 260");
+  //
+  TLine* l1 = new TLine(0,1,9.0,1.0);
+  l1->SetLineWidth(2);
+  l1->SetLineColor(kRed);
+  l1->SetLineStyle(2);
 
   TLegend *w1 = new TLegend(0.25,0.4,0.5,0.5);
   w1->SetLineColor(kWhite);
@@ -200,22 +231,26 @@ void MinuitBlastWaveCMSdataFit(){
       y1.clear();
       ey1.clear();
 
-      for (int pt = 2; pt < 13; pt++){
+      //ks pt strats at 0.3-0.4
+      //la pt starts at 0.6-0.8
+      //all correspond to pt = 0;
+
+      for (int pt = 0; pt < 14; pt++){
 
         x.push_back( ks_ptbincenter[pt] );
         ex.push_back(0.0);
-        y.push_back( ksSpectra[mult]->GetBinContent(pt+2) );
-        ey.push_back( ksSpectra[mult]->GetBinError(pt+2) );
-        
+        y.push_back( ksSpectra[mult]->GetBinContent(pt+3) );
+        ey.push_back( ksSpectra[mult]->GetBinError(pt+3) );
+        //ey.push_back( 0.1 );
       }
 
-      for(pt = 1; pt < 13; pt++){
+      for(pt = 0; pt < 14; pt++){
 
           x1.push_back( la_ptbincenter[pt] );
           ex1.push_back(0.0);
           y1.push_back( laSpectra[mult]->GetBinContent(pt+2) );
           ey1.push_back( laSpectra[mult]->GetBinError(pt+2) );
-        
+          //ey1.push_back( 0.1 );
       }
 
   /**
@@ -233,9 +268,9 @@ void MinuitBlastWaveCMSdataFit(){
       gMinuit[mult]->mnexcm("SET ERR", arglist, 1, 0);
 
       gMinuit[mult]->mnparm(0, "aka",    10,   0.1,  1,    10000, 0);
-      gMinuit[mult]->mnparm(1, "beta_s", 0.70, 0.01, 0.15, 1.0,   0);
-      gMinuit[mult]->mnparm(2, "Tkin",   0.15, 0.01, 0.05, 1.0,   0);
-      gMinuit[mult]->mnparm(3, "n",      1.0,  0.01, 0.1,  10.0,  0);
+      gMinuit[mult]->mnparm(1, "beta_s", 0.70, 0.01, 0.15,   1.0,   0);
+      gMinuit[mult]->mnparm(2, "Tkin",   0.15, 0.01, 0.05,   1.0,   0);
+      gMinuit[mult]->mnparm(3, "n",      1.0,  0.01, 0.1,    10.0,  0);
       gMinuit[mult]->mnparm(4, "aka1",   10,   0.1,  1,    10000, 0);
 
       gMinuit[mult]->mnexcm("MIGRAD",    arglist,  1,   0);
@@ -256,11 +291,11 @@ void MinuitBlastWaveCMSdataFit(){
    * define the fit function by using the parameters from fit:
    */
 
-      double xmin = 0.35;
-      double xmax = 2.0;
+      double xmin = 0.45;
+      double xmax = 2.3;
 
-      double xmin1 = 0.8;
-      double xmax1 = 3.3;
+      double xmin1 = 1.0;
+      double xmax1 = 4.0;
 
       f1Name.str("");
       f2Name.str("");
@@ -284,7 +319,7 @@ void MinuitBlastWaveCMSdataFit(){
       f2[mult]->SetParameter(3,n[mult]);
 
       c1->cd(mult+1);
-      gPad->SetLogy(1);
+      //gPad->SetLogy(1);
 
       ksSpectra[mult]->SetMarkerSize(1.3);
       ksSpectra[mult]->SetMarkerStyle(20);
@@ -307,11 +342,228 @@ void MinuitBlastWaveCMSdataFit(){
       f2[mult]->Draw("same");
       f1[mult]->Draw("same");
 
+      for(int pt = 0; pt < 14; pt++){
+      
+        double mass = 0.497;
+
+        double temp = 0.;
+        double mt = sqrt(ks_ptbincenter[pt]*ks_ptbincenter[pt]+mass*mass);
+
+        temp = aka[mult] * mt * integral(beta_s[mult],Tkin[mult],n[mult],ks_ptbincenter[pt],mt);
+
+        f1_hist[mult]->SetBinContent(pt+3, temp);
+
+      }
+
+      for(int pt = 0; pt < 14; pt++){
+      
+        double mass = 1.115;
+
+        double temp = 0.;
+        double mt = sqrt(la_ptbincenter[pt]*la_ptbincenter[pt]+mass*mass);
+
+        temp = aka1[mult] * mt * integral(beta_s[mult],Tkin[mult],n[mult],la_ptbincenter[pt],mt);
+
+        f2_hist[mult]->SetBinContent(pt+2, temp);
+
+      }
+
+      
+  }
+
+
+  w1->Draw("same");
+
+  TCanvas *p1 = new TCanvas("p1","p1",1,1,1200,400);
+
+  p1->Range(0,0,1,1);
+  TPad* pad1[8];
+
+  pad1[0] = new TPad("pad10", "pad10",0.0,   0.2, 0.3,   1);
+  pad1[1] = new TPad("pad11", "pad11",0.3,   0.2, 0.533, 1);
+  pad1[2] = new TPad("pad12", "pad12",0.533, 0.2, 0.766, 1);
+  pad1[3] = new TPad("pad13", "pad13",0.766, 0.2, 1.0,   1);
+
+  pad1[4] = new TPad("pad18", "pad18",0.0,      0.0, 0.3,   0.2);
+  pad1[5] = new TPad("pad19", "pad19",0.3,      0.0, 0.533, 0.2);
+  pad1[6] = new TPad("pad110", "pad110",0.533,  0.0, 0.766, 0.2);
+  pad1[7] = new TPad("pad111", "pad111",0.766,  0.0, 1.0,   0.2);
+
+  for(int i = 0; i < 8; i++){
+
+  pad1[i]->SetLeftMargin(0.0);
+  pad1[i]->SetRightMargin(0);
+  pad1[i]->SetTopMargin(0.0);
+  pad1[i]->SetBottomMargin(0);
+
+  //pad1[i]->SetLogy(1);
+  pad1[i]->Draw();
+
+  }
+
+  pad1[0]->SetLeftMargin(0.165);
+  pad1[4]->SetLeftMargin(0.165);
+
+  pad1[3]->SetRightMargin(0.05);
+  pad1[7]->SetRightMargin(0.05);
+
+  pad1[0]->SetTopMargin(0.02);
+  pad1[1]->SetTopMargin(0.02);
+  pad1[2]->SetTopMargin(0.02);
+  pad1[3]->SetTopMargin(0.02);
+
+  pad1[4]->SetBottomMargin(0.20);
+  pad1[5]->SetBottomMargin(0.20);
+  pad1[6]->SetBottomMargin(0.20);
+  pad1[7]->SetBottomMargin(0.20);
+
+  for(mult = 0; mult < 4; mult++){
+
+    pad1[mult]->cd();
+    pad1[mult]->SetLogy(1);
+    ksSpectra[mult]->Draw();
+    laSpectra[mult]->Draw("same");
+    f1[mult]->Draw("same");
+    f2[mult]->Draw("same");
+    ratio[mult]->Draw("same");
   }
 
   w1->Draw("same");
 
+  for(mult = 0; mult < 4; mult++){
 
+    pad1[mult+4]->cd();
+    f1_hist[mult]->Divide( ksSpectra[mult] );
+    f1_hist[mult]->SetMarkerStyle(20);
+    f1_hist[mult]->SetMarkerColor(kBlue);
+    f1_hist[mult]->SetMarkerSize(1.3);
+    f1_hist[mult]->SetLineColor(kWhite);
+    f1_hist[mult]->SetTitle(" ");
+    f1_hist[mult]->SetStats(kFALSE);
+    f1_hist[mult]->GetYaxis()->SetTitle("Fit/data");
+    f1_hist[mult]->GetXaxis()->SetTitle("p^{}_{T} (GeV/c)");
+    f1_hist[mult]->GetYaxis()->SetTitleSize(0.09);
+    f1_hist[mult]->GetYaxis()->SetTitleOffset(0.63);
+    f1_hist[mult]->GetYaxis()->SetLabelSize(0.09);
+    f1_hist[mult]->GetYaxis()->SetLabelOffset(0.025);
+    f1_hist[mult]->GetYaxis()->SetRangeUser(0.6,1.4);
+
+    f1_hist[mult]->GetXaxis()->SetTitleSize(0.09);
+    f1_hist[mult]->GetXaxis()->SetLabelSize(0.10);
+
+    f2_hist[mult]->Divide( laSpectra[mult] );
+    f2_hist[mult]->SetMarkerStyle(20);
+    f2_hist[mult]->SetMarkerColor(kBlack);
+    f2_hist[mult]->SetMarkerSize(1.3);
+    f2_hist[mult]->SetLineColor(kWhite);
+    f2_hist[mult]->SetTitle(" ");
+    f2_hist[mult]->SetStats(kFALSE);
+    f2_hist[mult]->GetYaxis()->SetTitle("Fit/data");
+    f2_hist[mult]->GetXaxis()->SetTitle("p^{}_{T} (GeV/c)");
+    f2_hist[mult]->GetYaxis()->SetLabelSize(0.09);
+    f2_hist[mult]->GetYaxis()->SetLabelOffset(0.025);
+    f2_hist[mult]->GetYaxis()->SetRangeUser(-10,40);
+    
+    f1_hist[mult]->Draw("P");
+    f2_hist[mult]->Draw("Psame");
+    l1->Draw("same");
+  }
+
+
+
+  TCanvas *p2 = new TCanvas("p2","p2",1,1,1200,400);
+
+  p2->Range(0,0,1,1);
+  TPad* pad2[8];
+
+  pad2[0] = new TPad("pad20", "pad20",0.0,   0.2, 0.3,   1);
+  pad2[1] = new TPad("pad21", "pad21",0.3,   0.2, 0.533, 1);
+  pad2[2] = new TPad("pad22", "pad22",0.533, 0.2, 0.766, 1);
+  pad2[3] = new TPad("pad23", "pad23",0.766, 0.2, 1.0,   1);
+
+  pad2[4] = new TPad("pad28", "pad28",0.0,      0.0, 0.3,   0.2);
+  pad2[5] = new TPad("pad29", "pad29",0.3,      0.0, 0.533, 0.2);
+  pad2[6] = new TPad("pad210", "pad210",0.533,  0.0, 0.766, 0.2);
+  pad2[7] = new TPad("pad211", "pad211",0.766,  0.0, 1.0,   0.2);
+
+  for(int i = 0; i < 8; i++){
+
+  pad2[i]->SetLeftMargin(0.0);
+  pad2[i]->SetRightMargin(0);
+  pad2[i]->SetTopMargin(0.0);
+  pad2[i]->SetBottomMargin(0);
+  pad2[i]->Draw();
+
+  }
+
+  pad2[0]->SetLeftMargin(0.165);
+  pad2[4]->SetLeftMargin(0.165);
+
+  pad2[3]->SetRightMargin(0.05);
+  pad2[7]->SetRightMargin(0.05);
+
+  pad2[0]->SetTopMargin(0.02);
+  pad2[1]->SetTopMargin(0.02);
+  pad2[2]->SetTopMargin(0.02);
+  pad2[3]->SetTopMargin(0.02);
+
+  pad2[4]->SetBottomMargin(0.20);
+  pad2[5]->SetBottomMargin(0.20);
+  pad2[6]->SetBottomMargin(0.20);
+  pad2[7]->SetBottomMargin(0.20);
+
+  for(mult = 0; mult < 4; mult++){
+
+    pad2[mult]->cd();
+    pad2[mult]->SetLogy(1);
+    ksSpectra[mult+4]->Draw();
+    laSpectra[mult+4]->Draw("same");
+    f1[mult+4]->Draw("same");
+    f2[mult+4]->Draw("same");
+    ratio[mult+4]->Draw("same");
+  }
+
+  w1->Draw("same");
+
+  for(mult = 4; mult < 8; mult++){
+
+    pad2[mult]->cd();
+    f1_hist[mult]->Divide( ksSpectra[mult] );
+    f1_hist[mult]->SetMarkerStyle(20);
+    f1_hist[mult]->SetMarkerColor(kBlue);
+    f1_hist[mult]->SetMarkerSize(1.3);
+    f1_hist[mult]->SetLineColor(kWhite);
+    f1_hist[mult]->SetTitle(" ");
+    f1_hist[mult]->SetStats(kFALSE);
+    f1_hist[mult]->GetYaxis()->SetTitle("Fit/data");
+    f1_hist[mult]->GetXaxis()->SetTitle("p^{}_{T} (GeV/c)");
+    f1_hist[mult]->GetYaxis()->SetTitleSize(0.09);
+    f1_hist[mult]->GetYaxis()->SetTitleOffset(0.63);
+    f1_hist[mult]->GetYaxis()->SetLabelSize(0.09);
+    f1_hist[mult]->GetYaxis()->SetLabelOffset(0.025);
+    f1_hist[mult]->GetYaxis()->SetRangeUser(0.6,1.4);
+
+    f1_hist[mult]->GetXaxis()->SetTitleSize(0.09);
+    f1_hist[mult]->GetXaxis()->SetLabelSize(0.10);
+
+    f2_hist[mult]->Divide( laSpectra[mult] );
+    f2_hist[mult]->SetMarkerStyle(20);
+    f2_hist[mult]->SetMarkerColor(kBlack);
+    f2_hist[mult]->SetMarkerSize(1.3);
+    f2_hist[mult]->SetLineColor(kWhite);
+    f2_hist[mult]->SetTitle(" ");
+    f2_hist[mult]->SetStats(kFALSE);
+    f2_hist[mult]->GetYaxis()->SetTitle("Fit/data");
+    f2_hist[mult]->GetXaxis()->SetTitle("p^{}_{T} (GeV/c)");
+    f2_hist[mult]->GetYaxis()->SetLabelSize(0.09);
+    f2_hist[mult]->GetYaxis()->SetLabelOffset(0.025);
+    f2_hist[mult]->GetYaxis()->SetRangeUser(-10,40);
+    
+    f1_hist[mult]->Draw("P");
+    f2_hist[mult]->Draw("Psame");
+    l1->Draw("same");
+
+  }
 
   TCanvas* s1 = new TCanvas();
 
@@ -366,21 +618,6 @@ void MinuitBlastWaveCMSdataFit(){
 
       g1[mult]->Draw("same");
   }
-
-
-
-  
-  
-  
-  
- 
-
-
-
-
- 
-
-
 
 
 }
